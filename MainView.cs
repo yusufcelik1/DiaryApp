56bac3379b89054b5b388338fc9d485f9448f628
yusufcelik1 <yusufcancelik9@gmail.com>
2023-08-26 13:56:50 +0300

    Deneme

---
+++ MainView.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Odev2
+{
+    public class MainView
+    {
+        public static void View()
+        {
+            Console.WriteLine("Welcome!\nPress 1 to log in, press 2 to register, press 3 to forgot password and press enter.");
+            string sayi = "";
+            ConsoleKeyInfo karakter;
+            do
+            {
+                karakter = Console.ReadKey(true);
+                if (karakter.Key != ConsoleKey.Backspace && karakter.Key != ConsoleKey.Enter)
+                {
+                    sayi += karakter.KeyChar;
+                    Console.Write(karakter.KeyChar);
+                }
+                else if (karakter.Key == ConsoleKey.Backspace && sayi.Length > 0)
+                {
+                    sayi = sayi.Substring(0, (sayi.Length - 1));
+                    Console.Write("\b \b");
+                }
+            }
+            while (karakter.Key != ConsoleKey.Enter || string.IsNullOrWhiteSpace(sayi));
+            Console.WriteLine();
+
+            if (!string.IsNullOrWhiteSpace(sayi))
+            {
+                int selection = Convert.ToInt32(sayi);
+                connenctionDB.connection = new SqlConnection(connenctionDB.connectionString);
+                connenctionDB.connection.Open();
+                switch (selection)
+                {
+                    case 1:
+                        User.Login();
+                        break;
+                    case 2:
+                        User.SignUp();
+                        break;
+                    case 3:
+                        User.ForgotPassword();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                        break;
+                }
+                connenctionDB.connection.Close();
+            }
+        }
+        public static void ShowMainMenu(string username)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Write a diary");
+            Console.WriteLine("2. Read diary");
+            Console.WriteLine("3. List All Diary Entires");
+            Console.WriteLine("4. Delete Diary Entry");
+            Console.WriteLine("5. Exit");
+            Console.Write("Make your selection:");
+
+            double val = 0;
+            string sayi = "";
+
+            ConsoleKeyInfo karakter;
+            do
+            {
+                karakter = Console.ReadKey(true);
+                if (karakter.Key != ConsoleKey.Backspace)
+                {
+                    bool kontrol = double.TryParse(karakter.KeyChar.ToString(), out val);
+                    if (kontrol)
+                    {
+                        sayi += karakter.KeyChar;
+                        Console.Write(karakter.KeyChar);
+                    }
+                }
+                else
+
+                {
+                    if (karakter.Key == ConsoleKey.Backspace && sayi.Length > 0)
+                    {
+                        sayi = sayi.Substring(0, (sayi.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                }
+            }
+            while (karakter.Key != ConsoleKey.Enter || string.IsNullOrWhiteSpace(sayi));
+            Console.WriteLine();
+
+            int selection = Convert.ToInt32(sayi);
+            switch (selection)
+            {
+                case 1:
+                   DiaryProperties.WriteDiaryEntry(username);
+                    break;
+                case 2:
+                    DiaryProperties.ReadDiaryEntry(username);
+                    break;
+                case 3:
+                    DiaryProperties.ListAllDiaryEntries(username);
+                    break;
+                case 4:
+                    DiaryProperties.DeleteDiaryEntry(username);
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    ShowMainMenu(username);
+                    break;
+            }
+        }
+    }
+}
+
---
+++ connenctionDB.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev2
+{
+    public class connenctionDB
+    {
+        static byte[] key = new byte[32]; // 256-bit key
+        static byte[] iv = new byte[16]; // 128-bit Initialization Vector
+        public static SqlConnection connection;
+        public static string connectionString = @"Server=DESKTOP-FLOJOJC\SQLEXPRESS; Database=dailyApp; Trusted_Connection=SSPI; MultipleActiveResultSets=true; TrustServerCertificate=true;";
+
+        public static bool IsDateValid(string input)
+        {
+            // Check if the input consists of only digits and dots
+            foreach (char c in input)
+            {
+                if (!char.IsDigit(c) && c != '.')
+                {
+                    return false;
+                }
+            }
+
+            // Split the input by dots
+            string[] parts = input.Split('.');
+
+            // Check if there are three parts
+            if (parts.Length != 3)
+            {
+                return false;
+            }
+
+            // Check if each part is a valid integer
+            foreach (string part in parts)
+            {
+                if (!int.TryParse(part, out _))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static string EncryptContent(string content)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(content);
+                        }
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string DecryptContent(string encryptedContent)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(encryptedContent)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
